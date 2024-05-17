a469467d1557cd548ad45d461270b6828fdc5d39
Thandu-darshana <tharindurathnasuriya@gmail.com>
2024-05-17 13:54:18 +0530

    Update index.js
    db connection

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,18 @@
 import express from "express"
+import mysql from "mysql"
 
 const app = express()
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Thandu123",
+    database: "transportmanagmentsystem"
+})
+
+app.get("/",(req,res) =>{
+    res.json("hello world")
+})
 
 app.listen(8800,()=>{
     console.log("server is running on port 8800")
